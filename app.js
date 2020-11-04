742ac4a5c112f47fb8b1045993cea4d167c47c7f
Trehan Sangpriya <trehansangpriya@gmail.com>
2020-11-04 14:18:14 +0530

    scroll animation

--- app.js
+++ app.js
@@ -8,3 +8,20 @@ tl.to('.intro', { y: '-100%', duration: 0.5 })
 tl.to('.intro2', { y: '-100%', duration: 1 })
 
 tl2.to('svg', { rotation: 360, transformOrigin: "center", repeat: -1, duration: 9, ease: Power0.easeNone })
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".sec2",
+        start: "bottom bottom",
+        end: "bottom -400px",
+        // markers: true,
+        scrub: true,
+        pin: true,
+    }
+})
+    .from('.sec2', { backgroundColor: 'black' })
+    .from('.txt1', { x: innerWidth * 1, offset: '-+=1' },)
+    .from('.txt2', { x: innerWidth * -1, offset: '-+=1' })
+    .from('.txt3', { x: innerWidth * 1, offset: '-+=1' })
+    .from('.txt4', { x: innerWidth * -1, offset: '-+=1' })
+    .from('.txt5', { x: innerWidth * 1, offset: '-+=1' })
